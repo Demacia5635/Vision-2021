db5905d2384ebda446df4023a7f5e466a38e4666
galronen211 <galronen211@gmail.com>
2021-01-13 19:53:11 +0200

    Created repository with a placeholder file
    Signed-off-by: galronen211 <galronen211@gmail.com>

---
+++ main.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+from networktables import NetworkTables
+import parameters
+
+
+def update_networktable(distance, angle):
+
+def detect_ball(frame):
+
+def init_camera():
+    
+
+if __name__ == "__main__":
+    
